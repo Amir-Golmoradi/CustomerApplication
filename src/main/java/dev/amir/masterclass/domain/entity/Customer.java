a9988ebb0cc6158fa0cd37aba1e7356ca29be7ad
Amir <LeoQueenzer2.0@gmail.com>
2024-08-11 18:13:09 +0330

    I'M BATMAN

---
+++ src/main/java/dev/amir/masterclass/domain/entity/Customer.java
@@ -0,0 +1,38 @@
+package dev.amir.masterclass.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Customer customer = (Customer) o;
+        return getId() != null && Objects.equals(getId(), customer.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
---
+++ src/test/java/dev/amir/masterclass/SpringMasterClassApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amir.masterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasterClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
